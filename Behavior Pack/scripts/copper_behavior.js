e65d091bdf818c5b538b5611cbf8088c75f26893
Kadoman18 <kbreinholt@icloud.com>
2025-07-16 18:21:02 -0400

    got damage function mostly working

--- Behavior Pack/scripts/copper_behavior.js
+++ Behavior Pack/scripts/copper_behavior.js
@@ -54,37 +54,28 @@ class GetMainhandInfo {
 };
 // Reduce Axe Durability Accounting for Unbreaking Enchantment
 // NOT WORKING ~~~~~~~
-function damageAxe(playerInfo) {
-  if (!playerInfo.item || !axeIds.includes(playerInfo.itemTypeId)) return;
+function damageAxe(mainhandInfo) {
+  if (!mainhandInfo.item || !axeIds.includes(mainhandInfo.itemTypeId)) return;
   // Get unbreaking enchantment level
-  const durability = playerInfo.item.getComponent('minecraft:durability');
-  const enchantable = playerInfo.item.getComponent('minecraft:enchantable');
-  const hasUnbreaking = enchantable.hasEnchantment('minecraft:unbreaking');
-  if (!hasUnbreaking) {
-    //Damage item
-    console.warn(`No Unbreaking Enchantment?!\nYou just got damaged!`)
-    return;
-  }
-  const unbreaking = enchantable.getEnchantment('minecraft:unbreaking');
-  const unbreakingLevel = unbreaking.level;
-  const damageChance = 1 / (unbreakingLevel + 1);
-  const damageRoll = Math.random();
-  console.warn(`\nHas Unbreaking: ${hasUnbreaking}\nLevel: ${unbreakingLevel}\nDamage Chance: ${damageChance}\nDamage Roll: ${damageRoll}`);
-  // Calculate chance to consume durability --- NOT WORKING.. Need to look at better documentation maybe..
-  if (damageRoll < damageChance) {
-    console.warn('UNLUCKY'); // THIS
-    const newAxe = playerInfo.item.clone(); // SHIT
-    newAxe.durability = durability.damage - 1; // FUCKIN
-    playerInfo.slot.setItem(newAxe); // SUCKS
-  } else {
-    console.warn('LUCKY');
-  }
-  // If the axe is broken, remove it
-  if (durability.damage >= durability.maxDurability) {
-    playerInfo.slot.setItem();
+  let unbreakingLevel = 0;
+  let cloneAxe = mainhandInfo.item.clone();
+  mainhandInfo.slot.setItem(cloneAxe);
+  let durabilityComponent = cloneAxe.getComponent('minecraft:durability');
+  const enchantableComponent = cloneAxe.getComponent('minecraft:enchantable');
+  const hasUnbreaking = enchantableComponent.hasEnchantment('minecraft:unbreaking');
+  if (hasUnbreaking) unbreakingLevel = enchantableComponent.getEnchantment('minecraft:unbreaking').level;
+  let damageChance = 1 / (unbreakingLevel + 1);
+  let damageRoll = Math.random();
+  // Calculate chance to consume durability
+  if (damageRoll > damageChance) return;
+  if (durabilityComponent.damage >= durabilityComponent.maxDamage) {
+    // If the axe is broken, remove it
+    mainhandInfo.slot.setItem();
     //Might not work because doesn't have player initialized..?
     player.playSound('random.break', { location: player.location });
-  }
+
+  };
+  durabilityComponent.damage++;
 };
 // Unified Custom Component Behavior
 const copperBehaviorComponent = {
@@ -100,25 +91,25 @@ const copperBehaviorComponent = {
   },
   // Interaction Logic
   onPlayerInteract({ block, player }) {
-    const playerInfo = new GetMainhandInfo(player);
+    const mainhandInfo = new GetMainhandInfo(player);
     const blockLocation = block.location;
     const waxOnParticleColor = new MolangVariableMap();
     const waxOffParticleColor = new MolangVariableMap();
     waxOnParticleColor.setColorRGB('variable.color', {red: 255, green: 180, blue: 0});
     waxOffParticleColor.setColorRGB('variable.color', {red: 150, green: 150, blue: 150});
-    if (!playerInfo.item) return;
+    if (!mainhandInfo.item) return;
     const currentBlockId = block.typeId;
     const blockState = block.permutation.getAllStates();
-    const itemIsAxe = axeIds.includes(playerInfo.itemTypeId);
-    const itemIsHoneycomb = playerInfo.itemTypeId === 'minecraft:honeycomb';
+    const itemIsAxe = axeIds.includes(mainhandInfo.itemTypeId);
+    const itemIsHoneycomb = mainhandInfo.itemTypeId === 'minecraft:honeycomb';
     // Waxing Logic
     if (itemIsHoneycomb && waxMap[currentBlockId]) {
       block.setPermutation(BlockPermutation.resolve(waxMap[currentBlockId], blockState));
-      if (playerInfo.item.amount > 1) {
-      const newHoneycombAmount = new ItemStack(playerInfo.itemTypeId, playerInfo.item.amount - 1)
-      playerInfo.slot.setItem(newHoneycombAmount)
+      if (mainhandInfo.item.amount > 1) {
+      const newHoneycombAmount = new ItemStack(mainhandInfo.itemTypeId, mainhandInfo.item.amount - 1)
+      mainhandInfo.slot.setItem(newHoneycombAmount)
       } else {
-        playerInfo.slot.setItem();
+        mainhandInfo.slot.setItem();
       };
       player.playSound("copper.wax.on", { location: player.location });
       for (let i = 0; i < 15; i++) {
@@ -143,7 +134,7 @@ const copperBehaviorComponent = {
         };
         player.spawnParticle('minecraft:wax_particle', particleLocation, waxOffParticleColor);
       };
-      damageAxe(playerInfo);
+      damageAxe(mainhandInfo);
       return;
     };
     // De-Oxidization Logic
@@ -158,7 +149,7 @@ const copperBehaviorComponent = {
         };
         player.spawnParticle('minecraft:wax_particle', particleLocation, waxOffParticleColor);
       };
-      damageAxe(playerInfo);
+      damageAxe(mainhandInfo);
       return;
     }
     }
