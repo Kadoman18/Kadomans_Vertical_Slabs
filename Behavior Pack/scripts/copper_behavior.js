d941e49813b2d79f1f84f9574e8cc26c40405372
Kadoman18 <kbreinholt@icloud.com>
2025-07-16 23:57:37 -0400

    added vanilla copper list for proximity oxidization

--- Behavior Pack/scripts/copper_behavior.js
+++ Behavior Pack/scripts/copper_behavior.js
@@ -1,5 +1,4 @@
 import {
-  world,
   system,
   BlockPermutation,
   ItemStack,
@@ -10,32 +9,115 @@ const oxidizeMap = {
   "kado:cut_copper_vertical_slab": "kado:exposed_cut_copper_vertical_slab",
   "kado:exposed_cut_copper_vertical_slab": "kado:weathered_cut_copper_vertical_slab",
   "kado:weathered_cut_copper_vertical_slab": "kado:oxidized_cut_copper_vertical_slab",
-};
+}
 // De-Oxidization Map
 const deoxidizeMap = {
   "kado:exposed_cut_copper_vertical_slab": "kado:cut_copper_vertical_slab",
   "kado:weathered_cut_copper_vertical_slab": "kado:exposed_cut_copper_vertical_slab",
   "kado:oxidized_cut_copper_vertical_slab": "kado:weathered_cut_copper_vertical_slab",
-};
+}
 // Waxing Map
 const waxMap = {
   "kado:cut_copper_vertical_slab": "kado:waxed_cut_copper_vertical_slab",
   "kado:exposed_cut_copper_vertical_slab": "kado:waxed_exposed_cut_copper_vertical_slab",
   "kado:weathered_cut_copper_vertical_slab": "kado:waxed_weathered_cut_copper_vertical_slab",
   "kado:oxidized_cut_copper_vertical_slab": "kado:waxed_oxidized_cut_copper_vertical_slab",
-};
+}
 // Un-Waxing Map
 const unwaxMap = {
   "kado:waxed_cut_copper_vertical_slab": "kado:cut_copper_vertical_slab",
   "kado:waxed_exposed_cut_copper_vertical_slab": "kado:exposed_cut_copper_vertical_slab",
   "kado:waxed_weathered_cut_copper_vertical_slab": "kado:weathered_cut_copper_vertical_slab",
   "kado:waxed_oxidized_cut_copper_vertical_slab": "kado:oxidized_cut_copper_vertical_slab",
-};
+}
+// Vanilla Copper Blocks List
+const vanillaCopperBlocks = [
+    "minecraft:copper_block",
+    "minecraft:exposed_copper",
+    "minecraft:weathered_copper",
+    "minecraft:oxidized_copper",
+    "minecraft:waxed_copper",
+    "minecraft:waxed_exposed_copper",
+    "minecraft:waxed_weathered_copper",
+    "minecraft:waxed_oxidized_copper",
+    "minecraft:cut_copper",
+    "minecraft:exposed_cut_copper",
+    "minecraft:weathered_cut_copper",
+    "minecraft:oxidized_cut_copper",
+    "minecraft:waxed_cut_copper",
+    "minecraft:waxed_exposed_cut_copper",
+    "minecraft:waxed_weathered_cut_copper",
+    "minecraft:waxed_oxidized_cut_copper",
+    "minecraft:cut_copper_stairs",
+    "minecraft:exposed_cut_copper_stairs",
+    "minecraft:weathered_cut_copper_stairs",
+    "minecraft:oxidized_cut_copper_stairs",
+    "minecraft:waxed_cut_copper_stairs",
+    "minecraft:waxed_exposed_cut_copper_stairs",
+    "minecraft:waxed_weathered_cut_copper_stairs",
+    "minecraft:waxed_oxidized_cut_copper_stairs",
+    "minecraft:cut_copper_slab",
+    "minecraft:exposed_cut_copper_slab",
+    "minecraft:weathered_cut_copper_slab",
+    "minecraft:oxidized_cut_copper_slab",
+    "minecraft:waxed_cut_copper_slab",
+    "minecraft:waxed_exposed_cut_copper_slab",
+    "minecraft:waxed_weathered_cut_copper_slab",
+    "minecraft:waxed_oxidized_cut_copper_slab",
+    "minecraft:chiseled_copper",
+    "minecraft:exposed_chiseled_copper",
+    "minecraft:weathered_chiseled_copper",
+    "minecraft:oxidized_chiseled_copper",
+    "minecraft:waxed_chiseled_copper",
+    "minecraft:waxed_exposed_chiseled_copper",
+    "minecraft:waxed_weathered_chiseled_copper",
+    "minecraft:waxed_oxidized_chiseled_copper",
+    "minecraft:copper_grate",
+    "minecraft:exposed_copper_grate",
+    "minecraft:weathered_copper_grate",
+    "minecraft:oxidized_copper_grate",
+    "minecraft:waxed_copper_grate",
+    "minecraft:waxed_exposed_copper_grate",
+    "minecraft:waxed_weathered_copper_grate",
+    "minecraft:waxed_oxidized_copper_grate",
+    "minecraft:copper_door",
+    "minecraft:exposed_copper_door",
+    "minecraft:weathered_copper_door",
+    "minecraft:oxidized_copper_door",
+    "minecraft:waxed_copper_door",
+    "minecraft:waxed_exposed_copper_door",
+    "minecraft:waxed_weathered_copper_door",
+    "minecraft:waxed_oxidized_copper_door",
+    "minecraft:copper_trapdoor",
+    "minecraft:exposed_copper_trapdoor",
+    "minecraft:weathered_copper_trapdoor",
+    "minecraft:oxidized_copper_trapdoor",
+    "minecraft:waxed_copper_trapdoor",
+    "minecraft:waxed_exposed_copper_trapdoor",
+    "minecraft:waxed_weathered_copper_trapdoor",
+    "minecraft:waxed_oxidized_copper_trapdoor",
+    "minecraft:copper_bulb",
+    "minecraft:exposed_copper_bulb",
+    "minecraft:weathered_copper_bulb",
+    "minecraft:oxidized_copper_bulb",
+    "minecraft:waxed_copper_bulb",
+    "minecraft:waxed_exposed_copper_bulb",
+    "minecraft:waxed_weathered_copper_bulb",
+    "minecraft:waxed_oxidized_copper_bulb"//FGD,
+    //FGD"minecraft:copper_chest",
+    //FGD"minecraft:exposed_copper_chest",
+    //FGD"minecraft:weathered_copper_chest",
+    //FGD"minecraft:oxidized_copper_chest",
+    //FGD"minecraft:waxed_copper_chest",
+    //FGD"minecraft:waxed_exposed_copper_chest",
+    //FGD"minecraft:waxed_weathered_copper_chest",
+    //FGD"minecraft:waxed_oxidized_copper_chest"
+];
 // All Vanilla Axe Type IDs
 const axeIds = [
   "minecraft:wooden_axe",
   "minecraft:stone_axe",
-  // "minecraft:copper_axe",  - Fall Game Drop -
+  //FGD"minecraft:copper_axe",
   "minecraft:iron_axe",
   "minecraft:diamond_axe",
   "minecraft:netherite_axe",
@@ -51,7 +133,7 @@ class GetMainhandInfo {
     const itemTypeId = item.typeId;
     this.itemTypeId = itemTypeId;
   }
-};
+}
 function spawnParticles(player, block, particle, color) {
   const blockLocation = block.location;
   for (let particleCount = 0; particleCount < 15; particleCount++) {
@@ -59,9 +141,9 @@ function spawnParticles(player, block, particle, color) {
       x: blockLocation.x + Math.random(),
       y: blockLocation.y + Math.random(),
       z: blockLocation.z + Math.random(),
-    };
+    }
     player.spawnParticle(particle, particleLocation, color);
-  };
+  }
 }
 // Reduce Axe Durability Accounting for Unbreaking Enchantment
 function damageAxe(mainhandInfo, player) {
@@ -84,11 +166,11 @@ function damageAxe(mainhandInfo, player) {
     console.warn(`DAMAGED\nDamage: ${durabilityComponent.damage}\nMax Durability: ${durabilityComponent.maxDurability}`);
     durabilityComponent.damage++;
     return;
-  };
+  }
   // Axe is broken
   player.playSound('random.break', { location: player.location });
   mainhandInfo.slot.setItem();
-};
+}
 // Unified Custom Component Behavior
 const copperBehaviorComponent = {
   // Oxidization Logic (Triggered by minecraft:tick)
@@ -118,13 +200,13 @@ const copperBehaviorComponent = {
       block.setPermutation(BlockPermutation.resolve(waxMap[currentBlockId], blockState));
       if (mainhandInfo.item.amount == 1) {
         mainhandInfo.slot.setItem();
-      };
+      }
       const newHoneycombAmount = new ItemStack(mainhandInfo.itemTypeId, mainhandInfo.item.amount - 1)
       mainhandInfo.slot.setItem(newHoneycombAmount)
       player.playSound("copper.wax.on", { location: player.location });
       spawnParticles(player, block, 'minecraft:wax_particle', waxOnParticleColor);
       return;
-    };
+    }
     // Un-Waxing Logic
     if (itemIsAxe && unwaxMap[currentBlockId]) {
       block.setPermutation(BlockPermutation.resolve(unwaxMap[currentBlockId], blockState));
@@ -132,7 +214,7 @@ const copperBehaviorComponent = {
       spawnParticles(player, block, 'minecraft:wax_particle', waxOffParticleColor);
       damageAxe(mainhandInfo, player);
       return;
-    };
+    }
     // De-Oxidization Logic
     if (itemIsAxe && deoxidizeMap[currentBlockId]) {
       block.setPermutation(BlockPermutation.resolve(deoxidizeMap[currentBlockId], blockState));
@@ -142,7 +224,7 @@ const copperBehaviorComponent = {
       return;
     }
     }
-  };
+  }
 // Component Registration
 system.beforeEvents.startup.subscribe(({ blockComponentRegistry }) => {
   blockComponentRegistry.registerCustomComponent('kado:copper_behavior', copperBehaviorComponent);
